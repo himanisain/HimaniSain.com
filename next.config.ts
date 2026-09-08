4b7c0637c3cfe1d350e59b2f1deddc3747281034
Arjun Aditya <arjunaditya92006@gmail.com>
2026-09-08 14:52:15 +0530

    delete files

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Disable the "Powered-By: Next.js" response header.
-  poweredByHeader: false,
-};
-
-export default nextConfig;
